1508e91e97bcc7d9af7c5108f0cdc74ec1947c57
mhhf <mhhf@freenet.de>
2014-03-06 20:25:01 +0100

    infinite scroll remove loading if finished

--- client/views/blocks.js
+++ client/views/blocks.js
@@ -18,6 +18,8 @@ scrollHandler = function(){
       var cLimit;
       if( ( cLimit = Session.get('blockLimit') )-10 <= Blocks.find().count() ) 
         Session.set('blockLimit',cLimit + 10 );
+      else
+        target.remove();
     }
   
 }
